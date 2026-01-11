89996464b31be728f6bb486898971e493a2b1279
lapvan1404 <dtc235180109@ictu.edu.vn>
2026-01-11 23:36:12 +0700

    fix frontend api base url

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,7 +1,4 @@
-export const BASE_URL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:5000'
-    : 'https://webecommerce-fwri.onrender.com';
+export const BASE_URL = process.env.REACT_APP_API_URL;
 
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
